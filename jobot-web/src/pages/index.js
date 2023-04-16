f56480bd2b2047d014b4598ae9fd5cadc93d34fd
Saurabh Shetty <63945352+KeenBean024@users.noreply.github.com>
2023-04-16 15:07:26 +0000

    progrss at 48mins

--- jobot-web/src/pages/index.js
+++ jobot-web/src/pages/index.js
@@ -1,5 +1,23 @@
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div>Hello world</div>)
+  const [apiKey, setApiKey] = useState('');
+  const API_URL = "https://api.openai.com/v1/chat/completions"
+
+  return(
+  <div className='flex flex-col h-screen'>
+    <nav className='shadow p-2 flex flex-row justify-between items-center'>
+      <div className="text-xl font-bold">Jobot</div>
+      <div>
+        <input type='password' 
+        className="border p-1 rounded" 
+        onChange={e=> setApiKey(e.target.value)}
+        value={apiKey}
+        placeholder="Paste API key here.." />
+      </div>
+    </nav>
+
+    
+  </div>
+  );
 }
